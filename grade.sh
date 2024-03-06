eb0117494da7c71ffad39b6e03f7ce65db998182
Zhuoran Li <121198882+luoluobuli@users.noreply.github.com>
2024-03-06 10:47:32 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,65 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# Test if the file exists
+if [ -f "student-submission/ListExamples.java" ]; then # Magic number: ListExamples.java
+    echo "The correct file exists."
+else
+    echo "The correct file does not exist."
+    echo "Your grade: 0"
+    exit
+fi
+
+# Move the file into the new directory
+cp student-submission/*.java Test*.java grading-area
+
+# Test if the file compiles succesfully
+cd grading-area
+javac -cp $CPATH *.java
+compile_exit_code=$?
+
+if [ $compile_exit_code -ne 0 ]; then
+    echo "File did not compile successfully."
+    echo "Your grade: 0"
+    exit
+else
+    echo "File compiled successfully."
+
+    # Test if the file runs succesfully
+    java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+    run_exit_code=$?
+    cat output.txt
+
+    if [ $run_exit_code -ne 0 ]; then
+        echo "Program did not execute successfully."
+        echo "Your grade: 0"
+        exit
+    else
+        echo "Program executed successfully."
+    fi
+fi
+
+total_tests=0
+failures=0
+errors=0
+passed_tests=0
+
+success_msg=$(grep -o "OK ([0-9]* test)" output.txt)
+
+if [[ ! -z "$success_msg" ]]; then
+    total_tests=$(echo $success_msg | grep -o "[0-9]*")
+    passed_tests=$total_tests
+else
+    total_tests=$(grep -o "Tests run: [0-9]*" output.txt | awk '{print $3}')
+    failures=$(grep -o "Failures: [0-9]*" output.txt | awk '{print $2}')
+    errors=$(grep -o "Errors: [0-9]*" output.txt | awk '{print $2}')
+    
+    [[ -z "$total_tests" ]] && total_tests=0
+    [[ -z "$failures" ]] && failures=0
+    [[ -z "$errors" ]] && errors=0
+
+    passed_tests=$((total_tests - failures - errors))
+fi
+
+echo "Your grade: $passed_tests/$total_tests"
